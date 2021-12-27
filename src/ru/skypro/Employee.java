23e1d38e56e345ad5e5c3dbc84fa0642062575e1
Micelovecat <flora_bux@mail.ru>
2021-12-28 02:31:06 +0300

    Third commit

---
+++ src/ru/skypro/Employee.java
@@ -0,0 +1,83 @@
+package ru.skypro;
+import java.util.Objects;
+
+public class Employee {
+    private static int counter = 1;
+
+    private int id;
+    private final String firstName;
+    private final String lastName;
+    private final String middleName;
+    private int department;
+    private float salary;
+
+    public Employee(String firstName, String lastName, String middleName, int department, float salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.middleName = middleName;
+        this.department = department;
+        this.salary = salary;
+        this.id = counter++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName + " " + middleName;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(float salary){
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id && department == employee.department && Float.compare(employee.salary, salary) == 0 && firstName.equals(employee.firstName) && lastName.equals(employee.lastName) && middleName.equals(employee.middleName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, middleName, department, salary);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", department=" + department +
+                ", salary=" + salary +
+                '}';
+    }
+}
---
+++ src/ru/skypro/Medium.java
@@ -0,0 +1,9 @@
+package ru.skypro;
+
+public class Medium {
+    private static final Employee[] employees = new Employee[10];
+
+    public static void main(String[] args){
+
+    }
+}
